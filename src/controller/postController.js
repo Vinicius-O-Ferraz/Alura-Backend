1a3a0a136b7be28ca9cad8abd2251284d7042022
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-11-23 18:51:10 -0300

    final aula 4

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -1,4 +1,5 @@
 import {getTodosPosts,criarPost} from "../models/postModels.js";
+import fs from "fs"
 
 export async function listarPosts(req,res){
     const posts = await getTodosPosts();
@@ -9,6 +10,19 @@ export async function publicarPost(req,res) {
     const newPost = req.body;
     try{
         const createdPost = await criarPost(newPost);
+        res.status(200).json(createdPost);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({"erro" : "Sistema em manuntenção"});
+    }
+}
+
+export async function uploadImagem(req,res) {
+    const newPost = req.body;
+    try{
+        const createdPost = await criarPost(newPost);
+        const imgAtualizada = `uploads/${createdPost.insertedId}.png`
+        fs.renameSync(req.file.path, imgAtualizada);
         res.status(200).json(createdPost);
     }catch(error){
         console.log(error.message);
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,11 +1,25 @@
 import express from "express";
-import { listarPosts, publicarPost } from "../controller/postController.js";
+import multer from "multer"
+
+import { listarPosts, publicarPost,uploadImagem } from "../controller/postController.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) =>{
     app.use(express.json()); //Indicando que cada string recebida pelo express deve ser interpretada como json
     app.get("/posts",listarPosts);
 
     app.post("/posts", publicarPost);
+    app.post("/upload",upload.single("imagem"),uploadImagem)
 }
 
 export default routes;
